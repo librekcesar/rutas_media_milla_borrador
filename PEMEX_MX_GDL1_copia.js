7a4c069a71844d668f51cb9a50e0f4579e937aea
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-06-24 17:03:17 -0600

    Add files via upload

---
+++ PEMEX_MX_GDL1_copia.js
@@ -0,0 +1,12 @@
+var pemex_gdl1 = {
+"type": "FeatureCollection",
+"name": "PEMEX_MX_GDL1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Gasolinera Estación Aguacaliente II", "description": "Autopista México-Guadalajara KM 128+800 Atlacomulco, 61054 Contepec, Mich." }, "geometry": { "type": "Point", "coordinates": [ -100.1374605, 19.8789966, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera G500 (Autopista Federal MX/GDL)", "description": "Carr. Guadalajara - Morelia, 58870 Copándaro de Galeana, Mich." }, "geometry": { "type": "Point", "coordinates": [ -101.1896702, 19.8857013, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "GRUPO OCTANO Estación Trébol 2", "description": "Trebol 2 Sur 9603 Autopista Mexico - Guadalajara Km. 202 No. 650, Zinapecuaro - Guadalajara, 58951 Mich." }, "geometry": { "type": "Point", "coordinates": [ -100.792566, 19.9021418, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Morelia-gdl", "description": "47914 La Barca, Jal." }, "geometry": { "type": "Point", "coordinates": [ -102.5119207, 20.3223095, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Carretera la Piedad Kilometro 17, 59730 Mich." }, "geometry": { "type": "Point", "coordinates": [ -102.2089777, 20.1615804, 0.0 ] } }
+]
+}
---
+++ PEMEX_MX_SPGR2_copia.js
@@ -0,0 +1,12 @@
+var pemex_spgr2 ={
+"type": "FeatureCollection",
+"name": "PEMEX_MX_SPGR2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "México-cuernavaca 4367, San Pedro Mártir, Tlalpan, 14650 Ciudad de México, CDMX" }, "geometry": { "type": "Point", "coordinates": [ -99.1644726, 19.2687628, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera PEMEX 13009", "description": "Cuernavaca - Chilpancingo 100, 39016 Chilpancingo de los Bravo, Gro." }, "geometry": { "type": "Point", "coordinates": [ -99.5187863, 17.5944152, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex 04033", "description": "Blvd. Vicente Guerrero No. 543, Salubridad, 39096 Chilpancingo de los Bravo, Gro." }, "geometry": { "type": "Point", "coordinates": [ -99.4819556, 17.5154411, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Oficina de Caminos y Puentes Federales", "description": "Carretera Federal Mexico - Acapulco s/n, Real Hacienda, Paso Limonero, 39713 Acapulco de Juárez, Gro." }, "geometry": { "type": "Point", "coordinates": [ -99.8109887, 16.9236586, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex", "description": "México 95 Km 119.08, Xoxocotla, 62670 Mor." }, "geometry": { "type": "Point", "coordinates": [ -99.281794, 18.691846, 0.0 ] } }
+]
+}
